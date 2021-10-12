865b3c07d6d71064a5af89214a3ebfb4865724dd
Rasmus Ploug <rasmusploug@live.dk>
2021-10-12 22:30:49 +0200

    some other stuff, idk

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+  res.render('home', { title: 'motedu.' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about - motedu.' });
+});
+
+router.get('/teachers', function(req, res, next) {
+  res.render('teachers', { title: 'teachers - motedu.' });
+});
+
+router.get('/parents', function(req, res, next) {
+  res.render('parents', { title: 'parents - motedu.' });
 });
 
 router.get('/student_login', function(req, res, next) {
-  res.render('login_student', { title: 'Express' });
+  res.render('login_student', { title: 'log-in - motedu.' });
 });
+
+router.get('/teacher_login', function(req, res, next) {
+  res.render('login_teacher', { title: 'log-in - motedu.' });
+});
+
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'game - motedu.' });
+});
+
 
 module.exports = router;
