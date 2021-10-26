27b385815e96c7fdad27c01c654b7b0ff90fd424
Rasmus Ploug <rasmusploug@live.dk>
2021-10-26 19:56:19 +0200

    Kæft hvor det bare virker altså

--- game/Assets/User.cs
+++ game/Assets/User.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class User : MonoBehaviour
 {
-    public string username;
+    private string URI = "http://localhost:3000/";
+
+    public string _username;
+    public string _fname;
+    public string _lname;
+    public string _email;
+    public string _school;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,6 +28,46 @@ public class User : MonoBehaviour
 
     public void GetUsername(string LoggedUser)
     {
-        username = LoggedUser;
+        _username = LoggedUser;
+        StartCoroutine("GetUserInfo");
+    }
+    public IEnumerator GetUserInfo()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", _username);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(URI + "getuserinfo", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+                Debug.Log(www.downloadHandler.text);
+
+                if (www.downloadHandler.text == "Info not found")
+                {
+                    
+                }
+                else
+                {
+                    DeserializeJson(www.downloadHandler.text);
+                }
+
+
+            }
+        }
+    }
+    public void DeserializeJson(string rawjson)
+    {
+        //"{\"email\":\"raplo20@student.sdu.dk\",\"fname\":\"Rasmus\",\"lname\":\"Ploug\", \"school\":\"SDU - Odense\"}"
+        _email = rawjson.Substring(10, rawjson.IndexOf("fname") - 3 - 10);
+        _fname = rawjson.Substring(rawjson.IndexOf("fname") + 8, rawjson.IndexOf("lname") - 3 - (rawjson.IndexOf("fname") + 8));
+        _lname = rawjson.Substring(rawjson.IndexOf("lname") + 8, rawjson.IndexOf("school") - 4 - (rawjson.IndexOf("lname") + 8));
+        _school = rawjson.Substring(rawjson.IndexOf("school") + 9, rawjson.IndexOf("}") - 1 - (rawjson.IndexOf("school") + 9));
     }
 }
