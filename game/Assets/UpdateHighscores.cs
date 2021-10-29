8f86193a0bfbd9dc4b2a1f938f3a47d187c80634
Rasmus Ploug <rasmusploug@live.dk>
2021-10-29 12:47:18 +0200

    Emil is still a genius

--- game/Assets/UpdateHighscores.cs
+++ game/Assets/UpdateHighscores.cs
@@ -7,12 +7,9 @@ using UnityEngine.UI;
 
 public class UpdateHighscores : MonoBehaviour
 {
-    
-    SortedList<int, string> sortedList = new SortedList<int, string>();
-    List<int> ExpList = new List<int>();
-    List<String> UsernameList = new List<String>();
+    List<Student> StudentList = new List<Student>();
     User user;
-   
+
 
     int amountOfUsers;
     private string URI = "http://localhost:3000/";
@@ -20,6 +17,7 @@ public class UpdateHighscores : MonoBehaviour
     Text HighscoreBox;
     Text Username;
     Text YourRank;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +26,7 @@ public class UpdateHighscores : MonoBehaviour
         Username = GameObject.Find("PersonalScoreText").GetComponentInChildren<Text>();
 
         StartCoroutine("UpdateHighscore");
-        
+
     }
 
     // Update is called once per frame
@@ -49,7 +47,7 @@ public class UpdateHighscores : MonoBehaviour
                 Debug.Log(www.error);
             }
             else
-            {      
+            {
                 var response = www.downloadHandler.text;
                 if (response == "Info not found")
                 {
@@ -67,45 +65,44 @@ public class UpdateHighscores : MonoBehaviour
         amountOfUsers = GetCount(json);
         List<string> HighscoreList = new List<string>();
 
-        for(int i = amountOfUsers; i>0 ; i--)
+        for (int i = amountOfUsers; i > 0; i--)
         {
-                HighscoreList.Add(CutJson(json, ",", i));
-            
+            HighscoreList.Add(CutJson(json, ",", i));
+
         }
-        
+
         for (int i = 0; i < amountOfUsers; i++)
         {
-            int level = CutLevel(HighscoreList[i]);
+            int exp = CutExp(HighscoreList[i]);
             string username = CutUsername(HighscoreList[i]);
-            sortedList.Add(level, username);
-            ExpList.Add(level);
+
+            StudentList.Add(new Student(exp, username));
+
         }
-        ExpList.Sort();
-        ExpList.Reverse();
-        for (int i = 0; i < amountOfUsers; i++)
-        {
-            UsernameList.Add(sortedList[ExpList[i]]);           
-        }
+
+        StudentList.Sort();
+        StudentList.Reverse();
+
         UpdateHighscoreText();
     }
     public void UpdateHighscoreText()
     {
-        for(int i = 0; i <= 23; i++)
+        for (int i = 0; i <= 23; i++)
         {
             HighscoreBox = GameObject.Find("Highscore (" + i + ")").GetComponentInChildren<Text>();
 
             if (i < amountOfUsers)
             {
                 int level = 0;
-                int exp = ExpList[i];
+                int exp = StudentList[i].exp;
                 while (exp >= 100)
                 {
                     level++;
                     exp -= 100;
                 }
-                HighscoreBox.text = UsernameList[i] + "\nLevel " + level;
-                
-                if(UsernameList[i] == user._username)
+                HighscoreBox.text = StudentList[i].username + "\nLevel " + level;
+
+                if (StudentList[i].username == user._username)
                 {
                     user._personalRank = i + 1;
                 }
@@ -114,7 +111,7 @@ public class UpdateHighscores : MonoBehaviour
             {
                 HighscoreBox.text = "-\n-";
             }
-            
+
 
         }
         UpdatePersonalRank();
@@ -135,7 +132,7 @@ public class UpdateHighscores : MonoBehaviour
             index++;
             counter++;
             if (counter == i)
-            {              
+            {
                 break;
             }
         }
@@ -143,7 +140,7 @@ public class UpdateHighscores : MonoBehaviour
         string cut = uncut.Remove(uncut.IndexOf(","));
         return cut;
     }
-    public int CutLevel(string userString)
+    public int CutExp(string userString)
     {
         int level = int.Parse(userString.Substring(userString.IndexOf(":") + 2, userString.LastIndexOf("\"") - (userString.IndexOf(":") + 2)));
         //Debug.Log(level);
@@ -151,17 +148,39 @@ public class UpdateHighscores : MonoBehaviour
     }
     public string CutUsername(string userString)
     {
-        string username = userString.Substring(1,userString.IndexOf(":")-2);
+        string username = userString.Substring(1, userString.IndexOf(":") - 2);
         //Debug.Log(username);
         return username;
     }
     public int GetCount(string json)
-    
+
     {
         int count = new int();
         count = int.Parse(json.Substring(10, json.IndexOf(",") - 1 - 10));
 
         return count;
+    }
+}
+
+class Student : IComparable<Student>
+{
+    public int exp { get; set; }
+    public string username { get; set; }
+
+    public Student(int exp, string username)
+    {
+        this.exp = exp;
+        this.username = username;
+    }
+
+    public int CompareTo(Student other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        return Comparer<int>.Default.Compare(this.exp, other.exp);
     }
 }
 
